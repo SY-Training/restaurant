212cf94241058ae3260d13c88d2d3cfd1bcbea5f
sy <sy-unimportant@outlook.com>
2021-12-12 11:07:08 +0000

    Initial dom elements for home page set up. Heading, image and paragraph

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import Icon from './scone.png';
+import './style.css';
+
+(function component () {
+
+    const content = document.querySelector('#content');
+    const head = document.createElement('div');
+    head.setAttribute('id', 'head');
+
+    head.innerHTML = "<h1> Scone but not forgotten</h1>";
+    content.appendChild(head);
+
+    const sconeImg = new Image();
+    sconeImg.src = Icon;
+    content.appendChild(sconeImg);
+
+    const para = document.createElement('div');
+    para.setAttribute('id', 'para');
+    para.innerHTML = "<p>Jam then cream? Or cream then Jam?\nAre you a heathen, or a hero?</p>";
+    content.appendChild(para);
+
+})();
