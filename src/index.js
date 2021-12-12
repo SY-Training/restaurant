0896494680e841eb8b221ca7a41894da0dcfaeb6
sy <sy-unimportant@outlook.com>
2021-12-12 11:57:59 +0000

    added header with buttons. Will try splitting into modules.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import Icon from './scone.png';
 import './style.css';
 
-(function component () {
+(function headerFunc() {
+
+    const header = document.querySelector('#header');
+    
+    const home = document.createElement('button');
+    home.setAttribute('class', 'home');
+    home.innerText = "HOME";
+    header.appendChild(home);
+
+    const recipe = document.createElement('button');
+    recipe.setAttribute('class', 'recipe');
+    recipe.innerText = "RECIPE";
+    header.appendChild(recipe);
+
+    const about = document.createElement('button');
+    about.setAttribute('class', 'about');
+    about.innerText = "ABOUT"
+    header.appendChild(about);
+})();
+
+
+(function contentFunc () {
+
+    
 
     const content = document.querySelector('#content');
-    const head = document.createElement('div');
-    head.setAttribute('id', 'head');
 
-    head.innerHTML = "<h1> Scone but not forgotten</h1>";
-    content.appendChild(head);
+    const heading = document.createElement('div');
+    heading.setAttribute('id', 'head');
+    heading.innerHTML = "<h1> Scone but not forgotten</h1>";
+    content.appendChild(heading);
 
     const sconeImg = new Image();
     sconeImg.src = Icon;
